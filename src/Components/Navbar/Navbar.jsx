0568571d7c04626fd7b07664a9975489149a23ff
impressivecoder1 <rohanrahman251@gmail.com>
2026-04-03 02:47:30 +0600

    fake data  added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { NavLink } from 'react-router';
 
 const Navbar = () => {
-    return (
-        <div className="navbar bg-base-100 shadow-sm">
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -11,39 +12,25 @@ const Navbar = () => {
       <ul
         tabIndex="-1"
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+        <NavLink to={`/`}>Home</NavLink>
+        <NavLink >Apps</NavLink>
+        <NavLink >Install</NavLink>
       </ul>
     </div>
     <a className="btn btn-ghost text-xl">daisyUI</a>
   </div>
   <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2 bg-base-100 w-40 z-1">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+    <ul className="menu menu-horizontal">
+      <NavLink className={`px-2`} to={`/`}>Home</NavLink>
+        <NavLink >Apps</NavLink>
+        <NavLink className={`px-2`}>Install</NavLink>
     </ul>
   </div>
   <div className="navbar-end">
     <a className="btn">Button</a>
   </div>
 </div>
-    );
+  );
 };
 
 export default Navbar;
