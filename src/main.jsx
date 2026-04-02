b59e4e4a24c52996521c29524bbcbd81f45be738
impressivecoder1 <rohanrahman251@gmail.com>
2026-04-03 01:59:58 +0600

    navbar from daisy

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import { RouterProvider } from 'react-router'
 import router from './Pages/Router/Router.jsx'
 
@@ -10,3 +9,4 @@ createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />,
   </StrictMode>,
 )
+  
