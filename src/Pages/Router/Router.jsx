1caae1769451463672eb04538df8f64c4920f574
impressivecoder1 <rohanrahman251@gmail.com>
2026-04-03 02:05:00 +0600

    footer from daisy and mainLayout added

--- src/Pages/Router/Router.jsx
+++ src/Pages/Router/Router.jsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router";
 import Home from "../Home/Home";
+import MainLayout from "../../Components/Layouts/MainLayout";
 
 const router = createBrowserRouter([
     {
         path: `/`,
-        element: <Home></Home>
+        element: <MainLayout></MainLayout>
     }
 ])
 export default router
