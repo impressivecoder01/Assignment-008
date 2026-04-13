0b0922df01e1ef52c939d0dc14506044cad60b9e
impressivecoder1 <rohanrahman251@gmail.com>
2026-04-14 01:18:40 +0600

    banner quite done

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router';
 const Banner = () => {
     return (
         <div className='flex flex-col items-center justify-center w-5xl mx-auto'>
-            
+
             <h1 className='text-3xl text-center'>We Build <br /> <span className='text-[#935eed]'>Productive</span> Apps</h1>
             <h5 className='text-center'>At HERO.IO, we craft innovative apps designed to make everyday life simpler, smarter, and more exciting.Our goal is to turn your ideas into digital experiences that truly make an impact.</h5>
             <div className='flex gap-4'>
@@ -13,6 +13,29 @@ const Banner = () => {
             </div>
             <div>
                 <img src={'../../assets/hero.png'} alt="" />
+            </div>
+            <div className='bg-[#8a50ed] w-full'>
+                <div>
+                    <h1 className='text-center text-6xl text-white font-extrabold pb-3'>Trusted By Millions, Built For You</h1>
+                    <div className='flex justify-between text-white px-2'>
+                        <div className='flex flex-col items-center'>
+                            <h6 >Total Downloads</h6>
+                            <h1 className='text-5xl font-bold '>29.99M</h1>
+                            <h6>21% more than last month</h6>
+                        </div>
+                        <div className='flex flex-col items-center'>
+                            <h6>Total Reviews</h6>
+                            <h1 className='text-5xl font-bold'>906K</h1>
+                            <h6>46% more than last month</h6>
+                        </div>
+                        <div className='flex flex-col items-center'>
+                            <h6>Active Apps</h6>
+                            <h1 className='text-5xl font-bold'>132+</h1>
+                            <h6>31 more will Launch</h6>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
     );
