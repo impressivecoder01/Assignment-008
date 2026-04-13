490df18eec16d55fd5140251b57a5c6dc3c14ba4
impressivecoder1 <rohanrahman251@gmail.com>
2026-04-14 02:29:47 +0600

    showed data

--- src/Components/Cards/TCard.jsx
+++ src/Components/Cards/TCard.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
-const TCard = () => {
+const TCard = ({data}) => {
     return (
-        <div>
-            
+        <div className="card bg-base-100 w-96 shadow-sm">
+            <figure>
+                <img
+                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                    alt="Shoes" />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">{data.companyName}</h2>
+                <p>{data.description}</p>
+                <div className="flex justify-around ">
+                   <p className='text-blue-950'>8M</p>
+                   <p className='text-blue-950'>8M</p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/TrendingApps/TrendingApps.jsx
+++ src/Pages/TrendingApps/TrendingApps.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import TCard from '../../Components/Cards/TCard';
 
 const TrendingApps = ({datas}) => {
     // console.log(datas)
     return (
         <div className='py-3'>
-            <h1>Trending Apps</h1>
+            <h1 className='text-center text-5xl py-5 font-extrabold'>Trending Apps</h1>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
                 {
-
+                    datas.map(data => <TCard data={data}></TCard>)
                 }
             </div>
         </div>
