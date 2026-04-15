a7044ae96d420e652ce18e2b1928f7bbe977e002
impressivecoder1 <rohanrahman251@gmail.com>
2026-04-16 01:44:13 +0600

    more data added

--- src/Pages/AllApplications/AllApplications.jsx
+++ src/Pages/AllApplications/AllApplications.jsx
@@ -11,12 +11,19 @@ const AllApplications = () => {
                 <h1 className='text-3xl font-semibold'>Our All Applications</h1>
                 <h6>Explore All Apps on the Market developed by us. We code for Millions</h6>
             </div>
+            <div className='flex items-center justify-between mb-4'>
                 <h1 className='font-bold'>{appData.length} Apps Found</h1>
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-                    {
-                        appData.map(data => <TCard key={data.id} data={data}></TCard>)
-                    }
-                </div>
+                <label className="input">
+                    <span className="label"></span>
+                    <input type="search" placeholder="search here" />
+                </label>
+
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                {
+                    appData.map(data => <TCard key={data.id} data={data}></TCard>)
+                }
+            </div>
         </div>
     );
 };
