d372ce10f7f21bc7c1c24340971d22fa8828e474
impressivecoder1 <rohanrahman251@gmail.com>
2026-04-16 23:35:27 +0600

    search function added

--- src/Pages/AllApplications/AllApplications.jsx
+++ src/Pages/AllApplications/AllApplications.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useLoadData from '../../Components/Hooks/useLoadData';
 import TCard from '../../Components/Cards/TCard';
 
 const AllApplications = () => {
+   
+ const [search, setSearch] = useState('')
+    console.log(search)
     const { appData } = useLoadData()
-    console.log(appData)
+    const term = search.trim().toLocaleLowerCase()
+    const searchedApps = term? appData.filter(app => app.companyName.toLocaleLowerCase().includes(term)) : appData
+    console.log(searchedApps)
     return (
         <div className=' w-11/12 mx-auto'>
             <div className='text-center my-5'>
@@ -12,16 +17,16 @@ const AllApplications = () => {
                 <h6>Explore All Apps on the Market developed by us. We code for Millions</h6>
             </div>
             <div className='flex items-center justify-between mb-4'>
-                <h1 className='font-bold'>{appData.length} Apps Found</h1>
+                <h1 className='font-bold'>{searchedApps.length} Apps Found</h1>
                 <label className="input">
                     <span className="label"></span>
-                    <input type="search" placeholder="search here" />
+                    <input value={search} onChange={(e)=> setSearch(e.target.value)} type="search" placeholder="search here" />
                 </label>
 
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
                 {
-                    appData.map(data => <TCard key={data.id} data={data}></TCard>)
+                    searchedApps.map(data => <TCard key={data.id} data={data}></TCard>)
                 }
             </div>
         </div>
