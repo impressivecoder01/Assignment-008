166f8335902776a4ddeedd583932ab9a7ad1e666
impressivecoder1 <rohanrahman251@gmail.com>
2026-04-22 23:42:50 +0600

    banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -5,8 +5,8 @@ const Banner = () => {
     return (
         <div className='flex flex-col items-center justify-center w-5xl mx-auto'>
 
-            <h1 className='text-3xl text-center'>We Build <br /> <span className='text-[#935eed]'>Productive</span> Apps</h1>
-            <h5 className='text-center'>At HERO.IO, we craft innovative apps designed to make everyday life simpler, smarter, and more exciting.Our goal is to turn your ideas into digital experiences that truly make an impact.</h5>
+            <h1 className='text-5xl font-bold text-center'>We Build <br /> <span className='text-[#935eed]'>Productive</span> Apps</h1>
+            <h5 className='text-center my-5 font-semibold'>At HERO.IO, we craft innovative apps designed to make everyday life simpler, smarter, and more exciting.Our goal is to turn your ideas into digital experiences that truly make an impact.</h5>
             <div className='flex gap-4'>
                 <Link className='btn font-bold' to={`https://play.google.com/store/games?hl=en`} target="_blank">Google Play</Link>
                 <Link className='btn font-bold' to={`https://www.apple.com/app-store/`} target="_blank">App Store</Link>
